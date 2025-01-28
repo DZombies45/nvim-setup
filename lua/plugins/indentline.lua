2017157ed5b5c67dd64d44a57c058553633b5adf
DZombies45 <dewan.dinda@gmail.com>
2025-01-28 10:45:40 +0700

    setting update

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,11 +1,10 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
-	event = { "BufReadPre", "BufNewFile" },
 	main = "ibl",
 	opts = {
-		indent = {
-			char = "┊",
-		},
+		--indent = {
+		--	char = "┊",
+		--},
 		scope = {
 			show_start = true,
 			show_end = true,
